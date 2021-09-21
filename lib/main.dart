71aeea5d61cb9291dc18cdc10e103013724a60bc
Sandun01 <sandunlakshitha06@gmail.com>
2021-09-21 17:58:43 +0530

    modified navigator

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import './screens/splash_screen.dart';
 import './screens/connection_failed.dart';
 import 'screens/home_screeen.dart';
-import './widgets/bottom_nav_bar.dart';
+import 'screens/main_screen.dart';
 import './screens/category_screen.dart';
 import './screens/login_screen.dart';
 // import './screens/register_screen.dart';
@@ -12,49 +12,59 @@ import './screens/welcome_screen.dart';
 import './screens/my_advertisements.dart';
 import './screens/view_advertisement.dart';
 
+//navigation key
+GlobalKey<NavigatorState> mainNavigatorKey = GlobalKey<NavigatorState>();
+
 void main() {
   Color appPrimaryColor = const Color(0xff0094FF);
   Color appPrimaryColorDark = const Color(0xff0077CD);
 
-  runApp(MaterialApp(
-    theme: ThemeData(
-      //theme color
-      primaryColor: appPrimaryColor,
-      primaryColorDark: appPrimaryColorDark,
+  runApp(
+    MaterialApp(
+      theme: ThemeData(
+        //theme color
+        primaryColor: appPrimaryColor,
+        primaryColorDark: appPrimaryColorDark,
 
-      //font
-      fontFamily: 'Averta',
+        //font
+        fontFamily: 'Averta',
+      ),
+      debugShowCheckedModeBanner: false,
+
+      //navigation key
+      navigatorKey: mainNavigatorKey,
+
+      // home: MyAdvertisements(),
+      home: SplashScreen(),
+      routes: {
+        //data routes
+        '/home': (ctx) => Home(),
+        '/view-add': (ctx) => ViewAdvertisement(),
+        '/edit-add': (ctx) => EditAdvertisement(),
+
+        //normal routes
+        '/post-add': (ctx) => PostAdvertisement(),
+        '/login': (ctx) => Login(),
+        // '/register': (ctx) => Login(),
+
+        '/connection-failed': (ctx) => ConnectionFailed(),
+      },
+      //if rout not found
+      onGenerateRoute: (settings) {
+        print(settings.arguments);
+        return MaterialPageRoute(
+          //need to change into the home
+          builder: (ctx) => ConnectionFailed(),
+        );
+      },
+      //if faild to build screen
+      onUnknownRoute: (settings) {
+        print(settings.arguments);
+        return MaterialPageRoute(
+          //need to change into the error page
+          builder: (ctx) => ConnectionFailed(),
+        );
+      },
     ),
-    debugShowCheckedModeBanner: false,
-    home: Home(),
-    routes: {
-      //data routes
-      '/home': (ctx) => Home(),
-      '/view-add': (ctx) => ViewAdvertisement(),
-      '/edit-add': (ctx) => EditAdvertisement(),
-
-      //normal routes
-      '/post-add': (ctx) => PostAdvertisement(),
-      '/login': (ctx) => Login(),
-      // '/register': (ctx) => Login(),
-
-      '/connection-failed': (ctx) => ConnectionFailed(),
-    },
-    //if rout not found
-    onGenerateRoute: (settings) {
-      print(settings.arguments);
-      return MaterialPageRoute(
-        //need to change into the home
-        builder: (ctx) => ConnectionFailed(),
-      );
-    },
-    //if faild to build screen
-    onUnknownRoute: (settings) {
-      print(settings.arguments);
-      return MaterialPageRoute(
-        //need to change into the error page
-        builder: (ctx) => ConnectionFailed(),
-      );
-    },
-  ));
+  );
 }
